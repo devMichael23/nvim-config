b400bfc91780b5fca473aec1d31f43d1b93dcaf1
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-04-02 12:05:06 +0300

    [lua][settings] In base.lua add mor options $ comments

--- lua/settings/base.lua
+++ lua/settings/base.lua
@@ -1,7 +1,38 @@
 local opt = vim.opt
+local cmd = vim.cmd
 
-opt.colorcolumn = '200'
-opt.cursorline = true
-opt.spelllang = { 'en_us', 'ru' }
-opt.number = true
-opt.relativenumber = true
+
+
+
+------------------------------------------------------------------------------
+-- Main options
+------------------------------------------------------------------------------
+opt.colorcolumn = '200'              -- Separate at 200 symbols
+opt.cursorline = true               -- Highlighting a line with a cursor
+opt.spelllang = { 'en_us', 'ru' }   -- Dictionary
+opt.number = true 		            -- On line number
+-- opt.relativenumber = true 	        -- Highlighting a line with a cursor
+opt.splitright = true               -- Vertical split 
+opt.splitbelow = true               -- Horizontal split
+
+
+------------------------------------------------------------------------------
+-- Tabs options
+------------------------------------------------------------------------------
+cmd([[
+filetype indent plugin on
+syntax enable
+]])
+opt.expandtab = true      -- Use spaces instead of tabs
+opt.shiftwidth = 4        -- Shift 4 spaces when tab
+opt.tabstop = 4           -- 1 tab == 4 spaces
+opt.smartindent = true    -- Autoindent new lines
+
+
+------------------------------------------------------------------------------
+-- IDE options
+------------------------------------------------------------------------------
+-- Remembers where nvim last edited the file
+cmd [[
+autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+]]
