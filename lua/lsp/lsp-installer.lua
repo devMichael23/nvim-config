1ed6edb49408b3072a22e6adb780d945975603d3
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-05-19 11:40:09 +0300

    Added LSP server for C, Cmake & Python

--- lua/lsp/lsp-installer.lua
+++ lua/lsp/lsp-installer.lua
@@ -14,6 +14,31 @@ lsp_installer.on_server_ready(function(server)
          opts = vim.tbl_deep_extend('force', sumneko_opts, opts)
      end
 
+     if server.name == 'ccls' then
+        local ccls = require('lsp.settings.ccls')
+        opts = vim.tbl_deep_extend('force', ccls, opts)
+     end
+
+     if server.name == 'clang' then
+        local clang = require('lsp.settings.clang')
+        opts = vim.tbl_deep_extend('force', clang, opts)
+     end
+
+     if server.name == 'cmake' then
+        local cmake = require('lsp.settings.cmake')
+        opts = vim.tbl_deep_extend('force', cmake, opts)
+     end
+
+     if server.name == 'pylsp' then
+        local pylsp = require('lsp.settings.pylsp')
+        opts = vim.tbl_deep_extend('force', pylsp, opts)
+     end
+
+     if server.name == 'pyright' then
+        local pyright = require('lsp.settings.pyright')
+        opts = vim.tbl_deep_extend('force', pyright, opts)
+     end
+
     server:setup(opts)
 end)
 
