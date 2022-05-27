b91f6145a5d46f2006f1bf41c69141ea224be8f8
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2022-05-27 19:38:14 +0300

    Added list to treesitter install lang

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = "all",
+  ensure_installed = "c, cpp, cmake, lua, dockerfile, python, yaml",
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = true,
